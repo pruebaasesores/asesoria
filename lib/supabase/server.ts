0467a19c07e0c23d194cb3fb5efc92d230a6dda7
pruebaasesores <eltarotdericky@gmail.com>
2026-04-20 18:17:08 +0200

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -12,7 +12,9 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(
+  cookiesToSet: { name: string; value: string; options: any }[]
+) {
           cookiesToSet.forEach(({ name, value, options }) =>
             cookieStore.set(name, value, options)
           );
