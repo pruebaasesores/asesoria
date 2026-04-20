c2eeaf8b81373ad32fb0571c7706aef39cdb2069
pruebaasesores <eltarotdericky@gmail.com>
2026-04-20 19:15:17 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,17 @@
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body style={{background:'#0a0a0a',color:'#fff',fontFamily:'sans-serif'}}>
+      <body
+        style={{
+          background: "#0a0a0a",
+          color: "#fff",
+          fontFamily: "sans-serif",
+        }}
+      >
         {children}
       </body>
     </html>
